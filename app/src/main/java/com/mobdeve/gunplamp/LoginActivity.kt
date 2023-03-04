39ea939580e315c348981de0da49ef0c4bd43ed0
ginodelacruz08 <angelo_delacruz@dlsu.edu.ph>
2023-03-04 17:11:20 +0800

    login layout + sign up text button

--- app/src/main/java/com/mobdeve/gunplamp/LoginActivity.kt
+++ app/src/main/java/com/mobdeve/gunplamp/LoginActivity.kt
@@ -1,7 +1,11 @@
 package com.mobdeve.gunplamp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import com.mobdeve.gunplamp.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
@@ -10,10 +14,26 @@ class LoginActivity : AppCompatActivity() {
         const val INPUT_USER_KEY = "INPUT_USER_KEY"
 //        const val
     }
+
+    private val registerResultLauncher = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) {result: ActivityResult ->
+
+        if (result.resultCode == RESULT_OK) {
+            //TODO
+            Toast.makeText(this, "RESULT IS:" + result.data?.getStringExtra("firstName") + " " + result.data?.getStringExtra("password") + " " +result.data?.getStringExtra("username"), Toast.LENGTH_SHORT).show()
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         val viewBinding : ActivityLoginBinding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(viewBinding.root)
+
+        viewBinding.tvSignUp.setOnClickListener {
+            val intent = Intent(applicationContext, RegisterActivity::class.java)
+            registerResultLauncher.launch(intent)
+        }
     }
 }
