984bfd3a40338c68dad80032955b4f2266c2dc41
RonHideki <hideki.kawachi17@gmail.com>
2023-03-14 13:11:45 +0800

    testing create post image showing

--- app/src/main/java/com/mobdeve/gunplamp/CreatePostActivity.kt
+++ app/src/main/java/com/mobdeve/gunplamp/CreatePostActivity.kt
@@ -1,9 +1,13 @@
 package com.mobdeve.gunplamp
 
+import android.app.AlertDialog
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.mobdeve.gunplamp.databinding.ActivityCreatePostBinding
+import java.net.URI
 
 class CreatePostActivity : AppCompatActivity() {
 
@@ -12,6 +16,14 @@ class CreatePostActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val viewBinding : ActivityCreatePostBinding = ActivityCreatePostBinding.inflate(layoutInflater)
         setContentView(viewBinding.root)
+
+        val imagePostString = intent.getStringExtra("imagePost");
+        val imagePostURI = Uri.parse(imagePostString)
+
+
+        Toast.makeText(this, "IMAGE POST IS:" + imagePostString, Toast.LENGTH_SHORT).show()
+        viewBinding.ivImagePost.setImageURI(imagePostURI)
+
 
     }
 }
