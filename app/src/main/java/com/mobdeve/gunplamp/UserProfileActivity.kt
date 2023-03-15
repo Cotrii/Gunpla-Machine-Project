46461a11740a884723aa1a1ab51405b4e41fa1e4
ginodelacruz08 <angelo_delacruz@dlsu.edu.ph>
2023-03-15 23:04:55 +0800

    edit name username no save yet

--- app/src/main/java/com/mobdeve/gunplamp/UserProfileActivity.kt
+++ app/src/main/java/com/mobdeve/gunplamp/UserProfileActivity.kt
@@ -3,6 +3,7 @@ package com.mobdeve.gunplamp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.EditText
 import android.widget.TextView
 import androidx.activity.result.ActivityResult
@@ -20,23 +21,53 @@ class UserProfileActivity : AppCompatActivity() {
             //TODO
         }
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        var edit: Boolean = false
+        var editUsername : Boolean = false
+
         val viewBinding : ActivityUserProfileBinding = ActivityUserProfileBinding.inflate(layoutInflater)
         setContentView(viewBinding.root)
+
+        viewBinding.saveButton.visibility = View.INVISIBLE
 
         viewBinding.backButton.setOnClickListener{
             val intent = Intent(this@UserProfileActivity, HomeActivity::class.java)
             homeActivityLauncher.launch(intent)
         }
 
-        viewBinding.EditButton.setOnClickListener{
-            findViewById<EditText>(R.id.username).isEnabled = true
-            findViewById<EditText>(R.id.name).isEnabled = true
-            findViewById<TextView>(R.id.EditButton).text = "Save"
-            findViewById<TextView>(R.id.backButton).text = "Cancel"
+        viewBinding.editNameButton.setOnClickListener{
+            edit =! edit
+            if (edit){
+                findViewById<EditText>(R.id.name).isEnabled = true
+                viewBinding.saveButton.visibility = View.VISIBLE
+                viewBinding.editNameButton.setImageResource(R.drawable.baseline_cancel_24)
+            }
+            else{
+                findViewById<EditText>(R.id.name).isEnabled = false
+                viewBinding.saveButton.visibility = View.INVISIBLE
+                viewBinding.editNameButton.setImageResource(R.drawable.baseline_edit_24)
+
+            }
         }
+
+        viewBinding.editUsernameButton.setOnClickListener{
+            editUsername =! editUsername
+            if (editUsername){
+                findViewById<EditText>(R.id.username).isEnabled = true
+                viewBinding.saveButton.visibility = View.VISIBLE
+                viewBinding.editUsernameButton.setImageResource(R.drawable.baseline_cancel_24)
+            }
+            else{
+                findViewById<EditText>(R.id.username).isEnabled = false
+                viewBinding.saveButton.visibility = View.INVISIBLE
+                viewBinding.editUsernameButton.setImageResource(R.drawable.baseline_edit_24)
+
+            }
+        }
+
 
 
     }
